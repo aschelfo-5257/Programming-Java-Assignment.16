f796167610038463553283d60f5a066ea0932285
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-14 16:26:02 -0400

    Staff.java

--- Chapter 34:  Programming Exercise 34.1 /src /Staff.java
+++ Chapter 34:  Programming Exercise 34.1 /src /Staff.java
@@ -1,22 +1,24 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.sql.Sql;
 
 public class Staff extends Application {
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    // TODO Auto-generated method stub
+    @Override
+    public void start(Stage primaryStage) {
+        // Simple UI: a single label inside a vertical box
+        Label label = new Label("Staff");
+        VBox root = new VBox(label);
 
-    Scene scene = new Scene(name me, 400, 80);
-    primaryStage.setTitle("Staff");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+        Scene scene = new Scene(root, 400, 80);
+        primaryStage.setTitle("Staff");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    Launch(args);
-  }
+    public static void main(String[] args) {
+        launch(args);   // NOT “Launch”
+    }
 }
